7104aab5ac396ab79f1d8b7c5ac29820f53efeac
pjosephgorman <pjosephgorman@gmail.com>
2018-10-31 20:29:15 -0400

    This is the final product as of this date

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 See, I created a new GitHub Repo
+
+
+So, you want to understand my code.
+
+Firstly, take a look at the app.js page. That is essentially the heart of my code.
+It encompasses all of the javascript mechanisms, and is central to how the app works. 
+
+
+I begin by importing all of the necessary packages from other parts of my code, react, and images that
+I don't end up using. 
+
+Then, I set up an array of objects: these are going to be card objects, the very basis of my code. 
+They are an array of the cards values and id's that I use to properly display everything the user
+wants to be displayed. There are some starting ones to be used as stock examples. 
+
+Next, I set up the variable html portion of my code. Notice that it is wrapped in a function. This is so I can
+call the function in the render later on. The function's perpose is to prevent hard coding, and allow for
+the flexibility necessary for this project. The function loops through the array of card objects, and 
+for each one that matches the category given to the function call, it displays that card in the given category.
+
+After that, I set up my state, which consists of the last ID used in the array of objects, so that the program
+can properly assign the ID of whatever card the user wants to add. It also includes the array of cards, so that I can 
+change the array, delete from it, and add to it without hard coding, and so that anything the user adds to it or 
+takes away from it is reflected in the app.
+
+Next, I set up the various functions needed to do the things the app is supposed to do, like add a new card, delete 
+a card, move cards between categories, and add likes or dislikes to a card. 
+
+The first function is the moveRight function. It's job is to take whatever card it was clicked on, and switches the 
+given category to the one to the right of it. If it is all the way to the right, in the "Action Points" category, it
+doesn't change anything. To do this, the function puts the cards array through a .map, so that it can loop through and 
+target only the card that the move button was clicked on. It uses an if statement to check this. It then uses an 
+if/if else/else series of statements to decipher what category it is currently in, and appropriately change the category
+to the one to the right of it. It then returns the changed card to state, and render is called again to reload the positions 
+of the cards. 
+
+The second function is the moveLeft function. It is identical to the moveRight function, except it moves the card selected
+to the category to the left. 
+
+The third function is the delete function. It's job is to remove whatever card was clicked on from the card object array. 
+It does this very simply. It uses setState to change the state of the array, and uses a .filter to loop through the array
+return only the cards that were not clicked on. 
+
+The fourth function is the addItem function. It's job is to add a new card object to the array based on what category it 
+was clicked in. It starts by creating a new object, the "newCard" object, and setting the text to a stock text to be changed
+by the user later, the category to the category the button was clicked in, and the id to be one higher than the last ID of the
+state. It then sets state to include the newest card and to increment the last ID value by 1 to account for the new card.
+
+The fifth function is the like function. It's job is to increment the likes value of the targeted card object in the array. 
+To do this, it loops through the array using a .map, and targets the card that the button was clicked on. It then increments
+the likes value of that card object, sets the state to reflect the new likes value, and returns the card to the state. 
+
+The sixth and final function is the dislike function. It is the same as the like function, but it increments the dislike value
+of the desired card object. 
+
+Finally, the render section returns a set of html to set up the basics of the page, and call the html function for each category. I assign the desired category, it's title, and all of the functions to be used in the category. 
--- src/App.js
+++ src/App.js
@@ -2,25 +2,194 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let cards = [
+  {
+    id: 1,
+    category: 'Went Well',
+    text: 'We ate food', 
+    likes: 0,
+    dislikes: 0
+  },
+  {
+    id: 2,
+    category: 'To Improve',
+    text: 'Eat more food next time',
+    likes: 0,
+    dislikes: 0
+  },
+  {
+    id: 3,
+    category: 'Action Points',
+    text: 'bring food to next class',
+    likes: 0,
+    dislikes: 0
+  },
+  {
+    id: 4, 
+    category: 'Went Well',
+    text: 'We drank water',
+    likes: 0,
+    dislikes: 0
+  }
+];
+
+function CardCategory(props) {      
+  console.log(props.cards)    
+  return (
+                <div class="RetroBoardCategory RetroBoardCategory-1">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                <h2>{props.title}</h2>
+                <button type="button" class="button button-new"onClick={() => props.addItem(props.category)}>
+                  <span class="sr-only">Add to "Went Well"</span>+
+                </button>
+                {props.cards.map(card => {
+                  if (props.category === card.category){
+                    return(
+                      <div class="Retro">
+                      <label class="sr-only" for="retro-label-3">Enter retro text</label>
+                      <textarea id="retro-label-3" class="textbox" placeholder="Enter text here" rows="1">{card.text}</textarea>
+                      <div class="ButtonGroup">
+                        <button type="button" class="button button-left"onClick={() => props.moveLeft(card.id, card.category)}><span class="sr-only">Move Left</span>&lt;</button>
+                        <button type="button" class="button fa fa-thumbs-up" onCLick={() => props.like(card.id, card.likes)}><span>{props.likes}</span></button>
+                        <button type="button" class="button button-delete"onClick={() => props.delete(card.id)}><span class="sr-only">Delete</span>×</button>
+                        <button type="button" class="button fa fa-thumbs-down" onCLick={() => props.dislike(card.id, card.dislikes)}><span>{props.dislikes}</span></button>
+                        <button type="button" class="button button-right"onClick={() => props.moveRight(card.id, card.category)}><span class="sr-only">Move Right</span>&gt;</button>
+                      </div>
+                    </div>
+                    );
+
+                  }
+
+                })}
+
+                </div> 
+  );
+}
+
 class App extends Component {
+  state = {
+    lastId: 4,
+    cards: cards
+  }
+
+  moveRight = (id, category) => {
+    this.setState({
+      cards: this.state.cards.map(card => {
+        if (card.id === id){
+         if (card.category === 'Went Well') card.category = 'To Improve';
+         else if (card.category === 'To Improve') card.category = 'Action Points';
+         else card.category = 'Action Points';
+      }
+      return card;
+    })
+    });
+  }
+
+  moveLeft = (id, category) => {
+    this.setState({
+      cards: this.state.cards.map(card => {
+        if (card.id === id) {
+         if (card.category === 'Action Points') card.category = 'To Improve';
+         else if (card.category === 'To Improve') card.category = 'Went Well';
+          else card.category = 'Went Well';
+      }
+      return card;
+    })
+    });
+  }
+
+  delete = (id) => {
+    this.setState({
+      cards: this.state.cards.filter(card => {
+        return card.id !== id;
+      })
+    });
+  }
+
+  addItem = (category) => {
+    const {cards} = this.state;
+    const newCard  = {
+      id: this.state.lastId + 1,
+      category: category,
+      text: 'new words'
+    }
+    
+      this.setState({
+      cards:[...this.state.cards,newCard],
+      lastId: ++this.state.lastId
+    },()=> console.log(this.state))
+  }
+
+  like = (id, likes) => {
+    this.setState({
+      cards: this.state.cards.map(card => {
+        if (card.id === id) {
+        card.likes = card.likes + 1;
+        }
+        return card;
+      })
+    })
+}
+
+  dislike = (id, dislikes) => {
+    this.setState({
+      cards: this.state.cards.map(card => {
+        if (card.id === id) {
+        card.dislikes = card.dislikes + 1;
+        }
+        return card;
+      })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        
+      
+          <main class="content">
+            <h1>Retro Board</h1>
+            <div class="RetroBoad">
+      
+              <CardCategory
+              title = "Went Well"
+              category = "Went Well"
+              cards = {this.state.cards}
+              addItem = {this.addItem}
+              delete = {this.delete}
+              moveLeft = {this.moveLeft}
+              moveRight = {this.moveRight}
+              like = {this.like}
+              dislike = {this.dislike}
+              />
+      
+              <CardCategory
+              title = "To Improve"
+              category = "To Improve"
+              cards = {this.state.cards}
+              addItem = {this.addItem}
+              delete = {this.delete}
+              moveLeft = {this.moveLeft}
+              moveRight = {this.moveRight}
+              like = {this.like}
+              dislike = {this.dislike}
+              />
+                
+      
+              <CardCategory
+              title = "Action Points"
+              category = "Action Points"
+              cards = {this.state.cards}
+              addItem = {this.addItem}
+              delete = {this.delete}
+              moveLeft = {this.moveLeft}
+              moveRight = {this.moveRight}
+              like = {this.like}
+              dislike = {this.dislike}
+              />
+            </div>
+          </main>
+    
+        
     );
   }
 }
